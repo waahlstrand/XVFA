ef5b7d993ff5554cddb12fc7a34b38a968ac1619
Victor Wåhlstrand Skärström <victor.wahlstrand@gmaiil.com>
2024-07-03 13:56:10 +0200

    Removed unused MLP class

--- models/base.py
+++ models/base.py
@@ -8,20 +8,12 @@ from torchvision.ops import roi_align
 import lightning as L
 from models.vertebra.models import SingleVertebraClassifier
 from utils.evaluate import *
-
-class MLP(nn.Module):
-    """ Very simple multi-layer perceptron (also called FFN)"""
-
-    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
-        super().__init__()
-        self.num_layers = num_layers
-        h = [hidden_dim] * (num_layers - 1)
-        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
-
-    def forward(self, x):
-        for i, layer in enumerate(self.layers):
-            x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
-        return x
+from utils.types import Batch
+from models.backbones.DINO.util import box_ops
+from models.spine.matcher import HungarianMatcher
+from dataclasses import asdict
+import joblib
+import pandas as pd
 
 
 class Detector(L.LightningModule):
